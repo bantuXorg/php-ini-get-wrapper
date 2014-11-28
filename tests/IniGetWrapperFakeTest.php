2fdb7427d164d3b26bb1d73d2a5eeab121c10371
Andreas Fischer <af@bantuX.org>
2014-11-28 13:14:17 +0100

    Add authors entry to composer.json and change email address.

--- tests/IniGetWrapperFakeTest.php
+++ tests/IniGetWrapperFakeTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-* (c) Andreas Fischer <git@andreasfischer.net>
+* (c) Andreas Fischer <af@bantuX.org>
 *
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
